3fc4fb1a12a5c4d205a113af91e3590be9480984
SESA566613 <shantanu.priyadarshi@se.com>
2021-02-08 18:41:46 +0530

    Update queries.js

--- queries.js
+++ queries.js
@@ -12,7 +12,7 @@ const pool = new Pool({
   // ssl: isProduction,
 })
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  pool.query('SELECT * FROM users', (error, results) => {
     if (error) {
       throw error
     }
